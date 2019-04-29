be07f70e180afa7a8303662bf40e63e80e8ba6bc
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2019-04-29 16:45:46 -0600

    modificado

--- etcd/initk03.sh
+++ etcd/initk03.sh
@@ -1,18 +1,18 @@
 ETCD_VERSION=3.2
 TOKEN=my-etcd-token
 CLUSTER_STATE=new
-NAME_1=kub01
-NAME_2=kub02
-NAME_3=kub03
-HOST_1=10.0.0.21
-HOST_2=10.0.0.22
-HOST_3=10.0.0.23
+NAME_1=ubuntu
+NAME_2=ubuntu02
+NAME_3=ubuntu03
+HOST_1=192.168.1.10
+HOST_2=192.168.1.11
+HOST_3=192.168.1.12
 CLUSTER=${NAME_1}=http://${HOST_1}:2380,${NAME_2}=http://${HOST_2}:2380,${NAME_3}=http://${HOST_3}:2380
 DATA_DIR=/var/lib/etcd
 
-# For node 3
-NODE_NAME=${NAME_3}
-NODE_IP=${HOST_3}
+# For node 1
+NODE_NAME=${NAME_1}
+NODE_IP=${HOST_1}
 docker run --restart always -d \
   -p 2379:2379 \
   -p 2380:2380 \
